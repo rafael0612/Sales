caed9a7a4c0c6f04f866594c0ec77134c46cee61
rafael0612 <rafaelcallata@gmail.com>
2025-12-28 00:43:20 -0500

    update version packages and sdk a 10 and add migrations

---
+++ Sales.Backend/Migrations/20251228053848_UpdateMaxLengthCountryName.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sales.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMaxLengthCountryName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Countries",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Countries",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
--- Sales.Shared/Entities/Country.cs
+++ Sales.Shared/Entities/Country.cs
@@ -7,5 +7,6 @@ public class Country
     [Key]
     public int CountryId { get; set; }
     [Required]
+    [MaxLength(100)]
     public string Name { get; set; } = null!;
 }
